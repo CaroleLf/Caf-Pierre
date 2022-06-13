38316c30df6baa71ec717109cfb5ebf3b0b5017e
ETD <etd@info-205-04x.iut.bx1>
2022-06-13 15:00:49 +0200

    modif de path

--- script-df/test.py
+++ script-df/test.py
@@ -8,11 +8,12 @@ from geopy.extra.rate_limiter import RateLimiter
 import matplotlib.pyplot as plt
 import tqdm
 from tqdm._tqdm_notebook import tqdm_notebook
+from os import path
 
-pibPays = pd.read_csv("pib.csv", usecols=['Country Name', '2020'])
-populationPays = pd.read_csv("pop_totale.csv", usecols=['Country Name', 'Indicator Name', '2020'])
+pibPays = pd.read_csv(path.dirname(__file__) + "/pib.csv", usecols=['Country Name', '2020'])
+populationPays = pd.read_csv(path.dirname(__file__) + "/pop_totale.csv", usecols=['Country Name','2020'])
 paysVoulus = ["France", "Denmark", "Cote d'Ivoire", "China", "India", "United States"]
-paysContinent = pd.read_csv("all.csv", usecols=['Country Name', 'region'])
+paysContinent = pd.read_csv(path.dirname(__file__) + "/all.csv", usecols=['Country Name', 'region'])
 
 # exemple pour isin pib = pibData[pibData["Country Name"].isin(paysVoulus)]
 
